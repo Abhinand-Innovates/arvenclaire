844f9ac13f25a67df8b62e858c11626003c27b7a
Abhinand-Innovates <abhinand1403@gmail.com>
2025-05-25 17:06:25 +0530

    sendEmail editted

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,14 +16,6 @@ const sendEmail = async (email, otp) => {
       }
     })
 
-    transporter.verify((error, success) => {
-  if (error) {
-    console.log("Transporter verification error:", error);
-  } else {
-    console.log("Server is ready to send emails");
-  }
-});
-
 
     const info = await transporter.sendMail({
       from: process.env.NODEMAILER_EMAIL,
