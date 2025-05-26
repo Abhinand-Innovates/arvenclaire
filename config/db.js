715b9f62c997a16b1d26a04c6a2ed9f7d867f7a1
Abhinand-Innovates <abhinand1403@gmail.com>
2025-05-26 17:35:12 +0530

    admin authentication done

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try {
 
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Databade connected Successfully..!!");
+        console.log("Database connected Successfully..!!");
         
 
     } catch (error) {
