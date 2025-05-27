eedb75cf7ee1b81f762de716b97c119b621440dd
Abhinand-Innovates <abhinand1403@gmail.com>
2025-05-27 20:15:36 +0530

    admin dashboard done

--- models/user-schema.js
+++ models/user-schema.js
@@ -51,3 +51,5 @@ const userSchema = new Schema ({
 const User = mongoose.model("User",userSchema);
 
 module.exports = User;
+
+
--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/",userRoute);
 app.use("/",adminRoute);
 
 // app.get("/",(req,res) => {
-//   res.render("customer-listing")
+//   res.render("get-product")
 // })
 
 
